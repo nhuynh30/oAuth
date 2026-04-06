b9107a6ec16743dcf5c0b28cb6d62338c317c134
Quan Huynh <quanhuynh364@gmail.com>
2026-04-06 17:02:05 -0400

    setting up Passport Google OAuth configuration

---
+++ auth.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const GOOGLE_CLIENT_ID = process.env.CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/google/callback",
+    passReqToCallback: true
+},
+    function (request, accessToken, refreshToken, profile, done) {
+        return done(err, profile);
+    }
+
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+})
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+require('./auth');
+
+app.get('/', (req, res) => {
+    res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+app.get('/protect', (req, res) => {
+    res.send('Hello');
+});
+
+
+
+
+app.listen(3000);
